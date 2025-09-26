3b2da0c846ecde37feebbafceff1403e8252da30
“Raymond02620” <182052@cloud.rangitoto.school.nz>
2025-09-26 20:39:29 +1200

    just starting game 1

---
+++ Python/gameselection.py
@@ -0,0 +1,105 @@
+from tkinter import *
+import tkinter as tk
+import subprocess
+import sys
+
+# Helper functions for dynamic placement
+def relx(x): return x / 1920 # Assuming a base width of 1920 pixels
+def rely(y): return y / 1080 # Assuming a base height of 1080 pixels
+
+game_window = tk.Tk()
+
+screen_width = game_window.winfo_screenwidth()
+screen_height = game_window.winfo_screenheight()
+game_window.geometry(f"{screen_width}x{screen_height}")
+game_window.title("The Chong Games Compedium - Game Selection") #Sets the title of the window
+gm_icon = PhotoImage(file='Python/CHONG/Screenshot 2025-09-01 123022.gif') #Imports an image to use as the icon of the window
+gm_icon = gm_icon.subsample(2,2) #Resizes the image to be smaller by that factor
+game_window.iconphoto(True, gm_icon) #Sets the icon of the window
+game_window.config(background="#70B9E4") #Sets the background color of the window
+
+if len(sys.argv) > 1:
+    username = sys.argv[1]
+else:
+    username = "Guest"
+
+big_label = Label(game_window, 
+                  text="Welcome to The Chong Games Compedium, " + username + "!", 
+                  font=("MS Serif", 40), 
+                  fg ="#FFFFFF",
+                  relief=RAISED,
+                  bg="#70B9E4",
+                  bd = 10,
+                  padx = 10,
+                  pady = 20,)
+big_label.pack()
+
+small_label = Label(game_window, 
+                    text="Please select a game to play:", 
+                    font=("MS Serif", 25), 
+                    fg ="#FFFFFF",
+                    bg="#70B9E4",
+                    pady = 10,)
+small_label.pack()
+
+def enter_game1():
+    game_window.destroy()
+    subprocess.Popen(["python3", "Python/calculator.py", username])
+def enter_game2():
+    game_window.destroy()
+    subprocess.Popen(["python3", "Python/clicker.py", username])
+def enter_game3():
+    game_window.destroy()
+    subprocess.Popen(["python3", "Python/rps.py", username])
+
+
+game1_button = Button(game_window,
+                      text="Game 1: The Quest For the First Calculator",
+                        font=("MS Serif", 30),
+                        fg="#000000",
+                        bg="#FFFFFF",
+                        padx=20,
+                        pady=10,
+                        activebackground="#3B10D4",
+                        activeforeground="#ED0F0F",
+                        command=lambda: enter_game1())
+game1_button.place(relx=relx(550), rely=rely(300))
+
+game2_button = Button(game_window,
+                      text="Game 2: The Chong Clicker",
+                        font=("MS Serif", 30),
+                        fg="#000000",
+                        bg="#FFFFFF",
+                        padx=20,
+                        pady=10,
+                        activebackground="#3B10D4",
+                        activeforeground="#ED0F0F",
+                        command=lambda: enter_game2())
+game2_button.place(relx=relx(670), rely=rely(600))
+
+game3_button = Button(game_window,
+                      text="Game 3: Anarchy Rock Paper Scissors",
+                        font=("MS Serif", 30),
+                        fg="#000000",
+                        bg="#FFFFFF",
+                        padx=20,
+                        pady=10,
+                        activebackground="#3B10D4",
+                        activeforeground="#ED0F0F",
+                        command=lambda: enter_game3())
+game3_button.place(relx=relx(590), rely=rely(900))
+
+log_out_button = Button(game_window,
+                        text="Log Out",
+                        font=("MS Serif", 20),
+                        fg="#000000",
+                        bg="#FFFFFF",
+                        padx=10,
+                        pady=5,
+                        activebackground="#3B10D4",
+                        activeforeground="#ED0F0F",
+                        command=lambda: [game_window.destroy(), subprocess.Popen(["python3", "Python/menu.py"])])
+log_out_button.place(relx=relx(0), rely=rely(1000))
+   
+
+game_window.mainloop()
--- Python/menu.py
+++ Python/menu.py
@@ -1,9 +1,17 @@
 from tkinter import *
-import os
+import subprocess
 import json
 import time
 
-menu_window = Tk() #Creates/instanstiates a window 
+user_data = {}
+
+# Helper functions for dynamic placement
+def relx(x): return x / 1920 # Assuming a base width of 1920 pixels
+def rely(y): return y / 1080 # Assuming a base height of 1080 pixels
+
+
+
+menu_window = Tk() #Creates/instanstiates a window
 screen_width = menu_window.winfo_screenwidth()
 screen_height = menu_window.winfo_screenheight()
 menu_window.geometry(f"{screen_width}x{screen_height}")
@@ -13,23 +21,19 @@ mm_icon = mm_icon.subsample(2,2) #Resizes the image to be smaller by that factor
 menu_window.iconphoto(True, mm_icon) #Sets the icon of the window
 menu_window.config(background="#70B9E4") #Sets the background color of the window
 
-# Helper functions for dynamic placement
-def relx(x): return x / 1920
-def rely(y): return y / 1080
 
 label = Label(menu_window,
-              text = "🔥The Chong Games Compedium🔥",
-              font = ('MS Serif', 74),
-              fg="#D42D2D",
-              bg = "#70B9E4",
-              relief = RAISED,
-              bd = 10,
-              padx = 10,
-              pady = 20,
-              image = mm_icon,
-              compound = 'bottom',)
-label.place(relx=0.5, rely=0.08, anchor='n') #DYNAMICALLY CENTERED
-
+            text = "🔥The Chong Games Compedium🔥",
+            font = ('MS Serif', 74),
+            fg="#D42D2D",
+            bg = "#70B9E4",
+            relief = RAISED,
+            bd = 10,
+            padx = 10,
+            pady = 20,
+            image = mm_icon,
+            compound = 'bottom',)
+label.pack()
 exit_button = Button(menu_window,
                      text = "Exit",
                      font = ('MS Serif', 20),
@@ -43,7 +47,20 @@ exit_button = Button(menu_window,
                      command = menu_window.quit) #Quits the window when clicked
 exit_button.place(relx=relx(200), rely=rely(960)) #DYNAMIC
 
+dynamic_widgits = [] # List to keep track of dynamic widgets
+
+def clear_dynamic_widgits():  # Function to clear dynamic widgets
+    global dynamic_widgits # Access the global list
+    for widget in dynamic_widgits: # Iterate through the list of dynamic widgets
+        try: # Try to destroy each widget
+            widget.destroy()
+        except:# If it fails (e.g., widget already destroyed), just pass
+            pass
+    dynamic_widgits = []
+    menu_window.unbind('<Return>')  # Unbind Enter key
+
 def sign_up():
+    clear_dynamic_widgits()
     username_label = Label(menu_window,
                           text = "Enter your username below:",
                           font = ('MS Serif', 14),
@@ -69,6 +86,7 @@ def sign_up():
                           width = 30,
                           show="*")
     password_entry.place(relx=relx(150), rely=rely(780))
+
     submit_button = Button(menu_window,
                            text = "Submit",
                            font = ('MS Serif', 14),
@@ -79,33 +97,61 @@ def sign_up():
                            activebackground= "#23FF34",
                            padx = 10,
                            pady = 5,
-                           command = lambda: [submit(username_entry, password_entry), terminate_entries()])
+                           command = lambda: [submit(username_entry, password_entry), clear_dynamic_widgits()])
     submit_button.place(relx=relx(150), rely=rely(820))
-    def terminate_entries():
-        username_label.destroy()
-        username_entry.destroy()
-        password_label.destroy()
-        password_entry.destroy()
-        submit_button.destroy()
+    submit_button.focus_set() # Set focus to the submit button, which means it can be activated by pressing Enter
+    menu_window.bind('<Return>', lambda event: submit_button.invoke())
+    dynamic_widgits.extend([username_label, username_entry, password_label, password_entry, submit_button]) # Add widgets to the dynamic list (list operations)
+
+signup_feedback_label = Label(menu_window,
+                    text = "",
+                    font = ('MS Serif', 14),
+                    fg="#000000",
+                    bg = "#70B9E4",)
+
+signup_label_timer = None # Timer variable for clearing the label
 
 def submit(username_entry, password_entry):
-        username = (username_entry.get()).strip()
-        username = str(username)
-        password = (password_entry.get()).strip()
-        password = str(password)
-        password = str(password)
-        prevent_overwrite()
-        if username in user_data:
-            print("Username already exists!")
-        elif username == "" and password == "":
-            print("Please enter a username and password!")
-        else:
-            user_data[username] = password
-            with open('user_data.json', 'w') as f:
-                json.dump(user_data, f)
-            print("User data saved!")
-        username_entry.delete(0, END)
-        password_entry.delete(0, END)
+    global signup_label_timer
+    username = (username_entry.get()).strip()
+    username = str(username)
+    password = (password_entry.get()).strip()
+    password = str(password)
+    password = str(password)
+    prevent_overwrite()
+    if signup_label_timer:
+        menu_window.after_cancel(signup_label_timer) # Cancel previous timer if it exists
+        signup_label_timer = None
+    if username in user_data:
+        signup_feedback_label.config(text="Username already exists!")
+        signup_feedback_label.place(relx=relx(150), rely=rely(860))
+        menu_window.update()
+        signup_label_timer=menu_window.after(3000, lambda: signup_feedback_label.config(text="")) # Clear message after 3 seconds
+    elif username == "" and password == "":
+        signup_feedback_label.config(text="Please enter a username and password!")
+        signup_feedback_label.place(relx=relx(150), rely=rely(860))
+        menu_window.update()
+        signup_label_timer=menu_window.after(3000, lambda: signup_feedback_label.config(text="")) # Clear message after 3 seconds
+    elif username == "":
+        signup_feedback_label.config(text="Please enter a username!")
+        signup_feedback_label.place(relx=relx(150), rely=rely(860))
+        menu_window.update()
+        signup_label_timer=menu_window.after(3000, lambda: signup_feedback_label.config(text="")) # Clear message after 3 seconds
+    elif password == "":
+        signup_feedback_label.config(text="Please enter a password!")
+        signup_feedback_label.place(relx=relx(150), rely=rely(860))
+        menu_window.update()
+        signup_label_timer=menu_window.after(3000, lambda: signup_feedback_label.config(text="")) # Clear message after 3 seconds
+    else:
+        user_data[username] = password
+        with open('user_data.json', 'w') as f:
+            json.dump(user_data, f)
+        signup_feedback_label.config(text="Sign up successful!")
+        signup_feedback_label.place(relx=relx(150), rely=rely(860))
+        menu_window.update()
+        signup_label_timer=menu_window.after(3000, lambda: signup_feedback_label.config(text="")) # Clear message after 3 seconds
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
 
 def load_user_data():
     try:
@@ -123,8 +169,6 @@ def prevent_overwrite():
     except FileNotFoundError:
         pass
 
-user_data = {}
-
 sign_up_button = Button(menu_window,
                         text = "Sign Up",
                         font = ('MS Serif', 20),
@@ -138,19 +182,39 @@ sign_up_button = Button(menu_window,
                         pady = 5,)
 sign_up_button.place(relx=relx(200), rely=rely(620))
 
+login_label_timer = None # Timer variable for clearing the label
+login_feedback_label = Label(menu_window,
+                    text = "",
+                    font = ('MS Serif', 14),
+                    fg="#000000",
+                    bg = "#70B9E4",)
+
 def verify(username_entry, password_entry):
-    username = username_entry.get()
-    password = password_entry.get()
+    global login_label_timer
+    load_user_data()
+    username = (username_entry.get()).strip()
+    password = (password_entry.get()).strip()
+    if login_label_timer:
+        menu_window.after_cancel(login_label_timer) # Cancel previous timer if it exists
+        login_label_timer = None
     if username in user_data and user_data[username] == password:
-        print("Login successful!")
-        os.system("python gameselection.py")
-        menu_window.destroy()
+        login_feedback_label.config(text="Login successful! Logging in....")
+        login_feedback_label.place(relx=relx(800), rely=rely(860))
+        menu_window.update()
+        login_label_timer=menu_window.after(3000, lambda: login_feedback_label.config(text="")) # Clear message after 3 seconds
+        time.sleep(3) # Pause for a moment to let user see the success message
+        subprocess.Popen(["python3", "Python/gameselection.py", username]) # Pass username as argument
+        menu_window.quit()
     else:
-        print("Login failed!")
+        login_feedback_label.config(text="Invalid username or password!")
+        login_feedback_label.place(relx=relx(800), rely=rely(860))
+        menu_window.update()
+        login_label_timer=menu_window.after(3000, lambda: login_feedback_label.config(text="")) # Clear message after 3 seconds
     username_entry.delete(0, END)
     password_entry.delete(0, END)
 
 def login():
+    clear_dynamic_widgits()
     username_label = Label(menu_window,
                           text = "Enter your username below:",
                           font = ('MS Serif', 14),
@@ -176,6 +240,7 @@ def login():
                           width = 30,
                           show="*")
     password_entry.place(relx=relx(800), rely=rely(780))
+
     submit_button = Button(menu_window,
                            text = "Submit",
                            font = ('MS Serif', 14),
@@ -186,10 +251,12 @@ def login():
                            activebackground= "#23FF34",
                            padx = 10,
                            pady = 5,
-                           command = lambda: [ load_user_data(), verify(username_entry, password_entry),])
-    menu_window.bind("<Return>", lambda event: [load_user_data(), verify(username_entry, password_entry)])
-    submit_button.place(relx=relx(800), rely=rely(810))
+                           command = lambda: [ load_user_data(), verify(username_entry, password_entry), clear_dynamic_widgits(),])
 
+    submit_button.place(relx=relx(800), rely=rely(810))
+    submit_button.focus_set() # Set focus to the submit button, which means it can be activated by pressing Enter
+    menu_window.bind('<Return>', lambda event: submit_button.invoke())
+    dynamic_widgits.extend([username_label, username_entry, password_label, password_entry, submit_button]) # Add widgets to the dynamic list (list operations)
 login_button = Button(menu_window,
                       text = "Login",
                       font = ('MS Serif', 20),
@@ -201,8 +268,8 @@ login_button = Button(menu_window,
 login_button.place(relx=relx(900), rely=rely(620))
 
 def guest_play():
-    os.system("python gameselection.py")
     menu_window.destroy()
+    subprocess.Popen(["python3", "Python/gameselection.py"])
 
 guest_button = Button(menu_window,
                       text = "Play as Guest",
@@ -219,34 +286,39 @@ password_label = Label(menu_window,
                        font = ('MS Serif', 14),
                        fg="#000000",
                        bg = "#70B9E4",)
+
+forgot_label_timer = None # Timer variable for clearing the label
+
 def forgot_verify(username_entry):
     load_user_data()
     username = (username_entry.get()).strip()
+    username = str(username)
+    global forgot_label_timer
     try:
+        if forgot_label_timer:
+            menu_window.after_cancel(forgot_label_timer) # Cancel previous timer if it exists
+            forgot_label_timer = None
         if username in user_data:
-            password_label.config(text=f"Your username's password is: {user_data[username]}")
+            password_label.config(text=f"Your password is: {user_data[username]}")
             password_label.place(relx=relx(1500), rely=rely(920))
             menu_window.update()
-            time.sleep(5)
-            password_label.config(text="")
+            forgot_label_timer=menu_window.after(3000, lambda: password_label.config(text="")) # Clear message after 3 seconds
         elif username == "":
-            password_label.config(text="please enter a username!")
-            password_label.place(relx=relx(1500), rely=rely(920))
+            password_label.config(text="Please enter a username!")
             menu_window.update()
-            time.sleep(3)
-            password_label.config(text="")
+            forgot_label_timer=menu_window.after(3000, lambda: password_label.config(text="")) # Clear message after 3 seconds
         else:
-            password_label.config(text=f"Username not found!")
+            password_label.config(text="Username not found!")
             password_label.place(relx=relx(1500), rely=rely(920))
             menu_window.update()
-            time.sleep(3)
-            password_label.config(text="")
+            forgot_label_timer=menu_window.after(3000, lambda: password_label.config(text="")) # Clear message after 3 seconds
     except UnboundLocalError:
         print("No user data found!")
     username_entry.delete(0, END)
 
 def forgot_password_username():
     global username_label, username_entry, submit_button
+    clear_dynamic_widgits()
     username_label = Label(menu_window,
                           text = "Enter your username:",
                           font = ('MS Serif', 14),
@@ -269,12 +341,11 @@ def forgot_password_username():
                            activebackground= "#23FF34",
                            padx = 10,
                            pady = 5,
-                           command = lambda: [forgot_verify(username_entry), terminate_username_entries()])
+                           command = lambda: [forgot_verify(username_entry), clear_dynamic_widgits()])
     submit_button.place(relx=relx(1500), rely=rely(860))
-def terminate_username_entries():
-        username_label.destroy()
-        username_entry.destroy()
-        submit_button.destroy()
+    submit_button.focus_set() # Set focus to the submit button, which means it can be activated by pressing Enter
+    menu_window.bind('<Return>', lambda event: submit_button.invoke())
+    dynamic_widgits.extend([username_label, username_entry, submit_button]) # Add
 forgot_password_button = Button(menu_window,
                                 text = "Forgot Password?",
                                 font = ('MS Serif', 14),
@@ -285,7 +356,8 @@ forgot_password_button = Button(menu_window,
                                 activebackground= "#FF6923",
                                 padx = 10,
                                 pady = 5,
-                                command = lambda: forgot_password_username() )
+                                command = lambda: forgot_password_username())
 forgot_password_button.place(relx=relx(1500), rely=rely(960))
 
-menu_window.mainloop() #Runs the window infinitely until closed by user, listen for events.
+if __name__ == "__main__":
+    menu_window.mainloop()
